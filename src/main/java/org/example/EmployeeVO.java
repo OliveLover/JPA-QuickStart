dc375789a18cd977f83b367b0da2f29332b5699f
tiktokyi <tiktokyi@gmail.com>
2024-06-19 13:27:20 +0900

    Add : Lombok 추가

--- src/main/java/org/example/EmployeeVO.java
+++ src/main/java/org/example/EmployeeVO.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.sql.Timestamp;
 
+@Getter
+@Setter
+@ToString
 public class EmployeeVO {
     private Long id;
     private String name;
@@ -9,59 +16,4 @@ public class EmployeeVO {
     private String title;
     private String deptName;
     private Double salary;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Timestamp getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Timestamp startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDeptName() {
-        return deptName;
-    }
-
-    public void setDeptName(String deptName) {
-        this.deptName = deptName;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeeVO [id=" + id + ", name=" + name + ", startDate=" + startDate + ", title=" + title +
-                " , deptName=" + deptName + ", salary=" + salary + "]";
-    }
-
 }
