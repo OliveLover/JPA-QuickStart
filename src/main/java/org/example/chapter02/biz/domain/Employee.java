f013fe4f92cd680c0b922cb87502cacfa9bb81f0
tiktokyi <tiktokyi@gmail.com>
2024-06-21 20:50:38 +0900

    Mod : 자동전략 사용하기

--- src/main/java/org/example/chapter02/biz/domain/Employee.java
+++ src/main/java/org/example/chapter02/biz/domain/Employee.java
@@ -8,17 +8,9 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "S_EMP")
-@TableGenerator(name = "SEQ_GENERATOR",
-        table = "SHOPPING_SEQUENCES",
-        pkColumnName = "SEQ_NAME",
-        pkColumnValue = "EMP_SEQ",
-        valueColumnName = "NEXT_VALUE",
-        initialValue = 0,
-        allocationSize = 1)
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE,
-            generator = "SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
