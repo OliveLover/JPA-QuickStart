7f500756655b7d0257469788cae5c77d970b48ad
tiktokyi <tiktokyi@gmail.com>
2024-06-21 16:36:50 +0900

    Mod : @Access(AccessType.PROPERTY) 설정 해보기

--- src/main/java/org/example/chapter02/biz/domain/Employee.java
+++ src/main/java/org/example/chapter02/biz/domain/Employee.java
@@ -1,51 +1,80 @@
 package org.example.chapter02.biz.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.Date;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @ToString(exclude = {"searchCondition", "searchKeyword"})
 @Entity
 @Table(name = "S_EMP",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"NAME", "MAILID"})})
-@Access(AccessType.FIELD)
+@Access(AccessType.PROPERTY)
 public class Employee {
+    private Long id;
+    private String name;
+    private String mailId;
+    private Date startDate;
+    private String title;
+    private String deptName;
+    private Double salary;
+    private Double commissionPct;
+    private String searchCondition;
+    private String searchKeyword;
+
     @Id
     @Column(length = 7, nullable = false)
-    private Long id;
+    public long getId() {
+        return id;
+    }
 
     @Column(length = 25, nullable = false)
-    private String name;
+    public String getName() {
+        return name;
+    }
 
     @Column(length = 8, unique = true)
-    private String mailId;
+    public String getMailId() {
+        return mailId;
+    }
 
     @Column(name = "START_DATE")
     @Temporal(TemporalType.DATE)
-    private Date startDate;
+    public Date getStartDate() {
+        return startDate;
+    }
 
     @Column(length = 25)
-    private String title;
+    public String getTitle() {
+        return title;
+    }
 
     @Column(name = "DEPT_NAME", length = 30)
-    private String deptName;
+    public String getDeptName() {
+        return deptName;
+    }
 
     @Column(precision = 11, scale = 2)
-    private Double salary;
+    public Double getSalary() {
+        return salary;
+    }
 
     @Column(name = "COMMISSION_PCT", precision = 4, scale = 2,
-            columnDefinition = "double CHECK(commission_pct IN (10, 12.5, 15, 17.5, 20))")
-    private Double commissionPct;
+            columnDefinition = "double CHECK (commission_pct IN (10, 12.5, 15, 17.5, 20))")
+    public Double getCommissionPct() {
+        return commissionPct;
+    }
 
     @Transient
-    private String searchCondition;
+    public String getSearchCondition() {
+        return searchCondition;
+    }
 
     @Transient
-    private String searchKeyword;
+    public String getSearchKeyword() {
+        return searchKeyword;
+    }
 }
