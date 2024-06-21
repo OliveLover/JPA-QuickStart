5c5e07acd7bcafebbd70e2367364dc9de616a3ad
tiktokyi <tiktokyi@gmail.com>
2024-06-21 15:33:30 +0900

    Mod : @Column 어노테이션 설정 변경해보기

--- src/main/java/org/example/chapter02/biz/domain/Employee.java
+++ src/main/java/org/example/chapter02/biz/domain/Employee.java
@@ -8,25 +8,31 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "S_EMP",
-    uniqueConstraints = {@UniqueConstraint(columnNames = {"NAME", "MAILID"})})
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"NAME", "MAILID"})})
 public class Employee {
     @Id
+    @Column(length = 7, nullable = false)
     private Long id;
 
+    @Column(length = 25, nullable = false)
     private String name;
 
+    @Column(length = 8, unique = true)
     private String mailId;
 
-    @Column(name = "START_DATE")
+    @Column(name = "START_DATE", insertable = false)
     private Date startDate;
 
+    @Column(length = 25)
     private String title;
 
-    @Column(name = "DEPT_NAME")
+    @Column(name = "DEPT_NAME", length = 30)
     private String deptName;
 
+    @Column(precision = 11, scale = 2)
     private Double salary;
 
-    @Column(name = "COMMISSION_PCT")
+    @Column(name = "COMMISSION_PCT", precision = 4, scale = 2,
+            columnDefinition = "double CHECK(commission_pct IN (10, 12.5, 15, 17.5, 20))")
     private Double commissionPct;
 }
