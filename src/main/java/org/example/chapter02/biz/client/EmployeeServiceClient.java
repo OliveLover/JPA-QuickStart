0783d62c85d01c611e76fc9b4c41180e25189db8
tiktokyi <tiktokyi@gmail.com>
2024-06-28 00:32:13 +0900

    Mod : 식별자 값이 등록되지 않았을 때 merge가 persist와 같이 INSERT가 호출되는지 확인

--- src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
+++ src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
@@ -19,21 +19,8 @@ public class EmployeeServiceClient {
             employee.setName("둘리");
 
             tx.begin();
-            em.persist(employee);
+            em.merge(employee);
             tx.commit();
-
-            // 모든 엔티티를 분리 상태로 전환시킨다.
-            em.clear();
-
-            // 직원 엔티티 이름 수정
-            tx.begin();
-            employee.setName("똘리");
-            Employee mergedEmp = em.merge(employee);
-            tx.commit();
-
-            // 관리 상태 여부 확인
-            System.out.println("employee 관리 : " + em.contains(employee));
-            System.out.println("mergedEmp 관리 : " + em.contains(mergedEmp));
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
