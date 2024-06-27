56d56f086872f4137e84418e8ac7165cb77984bb
tiktokyi <tiktokyi@gmail.com>
2024-06-27 16:25:53 +0900

    Mod : 영속성 컨텐스트 관리상태(MANAGED, 영속상태)

--- src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
+++ src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
@@ -1,7 +1,6 @@
 package org.example.chapter02.biz.client;
 
 import org.example.chapter02.biz.domain.Employee;
-import org.example.chapter02.biz.domain.EmployeeId;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,12 +12,28 @@ public class EmployeeServiceClient {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter03");
         EntityManager em = emf.createEntityManager();
 
+        // 엔티티 트랜잭션 생성
+        EntityTransaction tx = em.getTransaction();
+
         try {
             // 회원 등록 요청
             Employee employee = new Employee();
             employee.setName("둘리");
+
+            // 트랜잭션 시작
+            tx.begin();
+
+            // 직원 등록
+            em.persist(employee);
+
+            // 트랜잭션 종료(COMMIT)
+            tx.commit();
+
         } catch (Exception e) {
             e.printStackTrace();
+
+            // 트랜잭션 종료(ROLLBACK)
+            tx.rollback();
         } finally {
             em.close();
             emf.close();
