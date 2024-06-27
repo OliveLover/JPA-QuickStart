0ae2e898b3dfa227aada87c27165588caf3654ef
tiktokyi <tiktokyi@gmail.com>
2024-06-28 00:43:21 +0900

    Mod : 식별자 값이 있는 경우merge는 SELECT로 먼저 조회 후 INSERT

--- src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
+++ src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
@@ -2,20 +2,19 @@ package org.example.chapter02.biz.client;
 
 import org.example.chapter02.biz.domain.Employee;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class EmployeeServiceClient {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter03");
         EntityManager em = emf.createEntityManager();
+        em.setFlushMode(FlushModeType.COMMIT);
         EntityTransaction tx = em.getTransaction();
 
         try {
             // 직원 엔티티 등록
             Employee employee = new Employee();
+            employee.setId(1L);
             employee.setName("둘리");
 
             tx.begin();
