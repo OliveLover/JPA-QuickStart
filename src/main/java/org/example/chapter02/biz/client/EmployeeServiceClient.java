e216fe0355ca905452b666661be31994abc761c5
tiktokyi <tiktokyi@gmail.com>
2024-06-28 00:18:40 +0900

    Mod : 엔티티 수정, 더치 체크와 UPDATE

--- src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
+++ src/main/java/org/example/chapter02/biz/client/EmployeeServiceClient.java
@@ -2,24 +2,28 @@ package org.example.chapter02.biz.client;
 
 import org.example.chapter02.biz.domain.Employee;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 public class EmployeeServiceClient {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter03");
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
         try {
             // 직원 검색
-            Employee findEmp1 = em.find(Employee.class, 1L);
-            Employee findEmp2 = em.find(Employee.class, 1L);
+            Employee findEmp = em.find(Employee.class, 1L);
 
-            // 객체의 동일성 비교
-            if(findEmp1 == findEmp2) {
-                System.out.println("검색된 두 객체는 동일한 객체다.");
-            }
+            // 직원 이름 변경
+            tx.begin();
+            findEmp.setName("똘리");
+            tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
+            tx.rollback();
         } finally {
             em.close();
             emf.close();
