7f4f1e77ee5857d9f3b61cfba642d343869ddbe2
tiktokyi <tiktokyi@gmail.com>
2024-06-30 01:06:46 +0900

    Mod : 다대일 단방향 매핑

--- src/main/java/org/example/chapter04/domain/Department.java
+++ src/main/java/org/example/chapter04/domain/Department.java
@@ -2,10 +2,17 @@ package org.example.chapter04.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "S_DEPT")
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long deptId;
 
+    @Column(length = 25, nullable = false)
     private String name;
 }
--- src/main/java/org/example/chapter04/domain/Employee.java
+++ src/main/java/org/example/chapter04/domain/Employee.java
@@ -2,12 +2,21 @@ package org.example.chapter04.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "S_EMP")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 25, nullable = false)
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "DEPT_ID")
     private Department dept;
 }
