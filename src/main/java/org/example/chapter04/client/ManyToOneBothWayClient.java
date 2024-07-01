32be166abcc39697bb8e3285c8e37b3c1435b9b2
tiktokyi <tiktokyi@gmail.com>
2024-07-02 00:23:51 +0900

    Mod : 영속성 전이 이해(TransientPropertyValueException)

--- src/main/java/org/example/chapter04/client/ManyToOneBothWayClient.java
+++ src/main/java/org/example/chapter04/client/ManyToOneBothWayClient.java
@@ -39,27 +39,14 @@ public class ManyToOneBothWayClient {
         // 부서 등록
         Department department = new Department();
         department.setName("개발부");
-        em.persist(department);
+//        em.persist(department);
 
         // 직원1 등록(Employee --> Department 참조)
         Employee employee1 = new Employee();
         employee1.setName("둘리");
         employee1.setDept(department);
         em.persist(employee1);
-
-        // 직원2 등록(Employee --> Department 참조)
-        Employee employee2 = new Employee();
-        employee2.setName("도우너");
-        employee2.setDept(department);
-        em.persist(employee2);
-
-        // Department.employeeList에 Employee 등록
-//        department.getEmployeeList().add(employee1);
-//        department.getEmployeeList().add(employee2);
-
-        System.out.println(department.getName() + "의 직원 수 : " +
-                department.getEmployeeList().size());
-
+        
         em.getTransaction().commit();
         em.close();
     }
