90384054909e6ee83fca79e92bdcc51028ac4be2
tiktokyi <tiktokyi@gmail.com>
2024-07-02 10:38:35 +0900

    Mod : 영속성 전이를 이용한 삭제(개별적 삭제 작업)

--- src/main/java/org/example/chapter04/client/ManyToOneBothWayClient.java
+++ src/main/java/org/example/chapter04/client/ManyToOneBothWayClient.java
@@ -6,13 +6,15 @@ import org.example.chapter04.domain.Employee;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class ManyToOneBothWayClient {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter04");
         try {
             dataInsert(emf);
-            dataSelect(emf);
+            dataDelete(emf);
+//            dataSelect(emf);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -30,6 +32,26 @@ public class ManyToOneBothWayClient {
             System.out.println(employee.getName() + "(" +
                     employee.getDept().getName() + ")");
         }
+    }
+
+    private static void dataDelete(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        // 부서 검색
+        Department department = em.find(Department.class, 1L);
+
+        // 부서에 등록된 직원 삭제
+        List<Employee> employeeList = department.getEmployeeList();
+        for (Employee employee : employeeList) {
+            em.remove(employee);
+        }
+
+        // 부서 삭제
+        em.remove(department);
+
+        em.getTransaction().commit();
+        em.close();
     }
 
     private static void dataInsert(EntityManagerFactory emf) {
