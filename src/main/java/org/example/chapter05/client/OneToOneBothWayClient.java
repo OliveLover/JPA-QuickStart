9bed6bad3bceca45bdd12feaa27a522f6b0cfedc
tiktokyi <tiktokyi@gmail.com>
2024-07-04 22:11:38 +0900

    Mod : 양방향 매핑 설정

--- src/main/java/org/example/chapter05/client/OneToOneBothWayClient.java
+++ src/main/java/org/example/chapter05/client/OneToOneBothWayClient.java
@@ -26,29 +26,33 @@ public class OneToOneBothWayClient {
     private static void dataSelect(EntityManagerFactory emf) {
         EntityManager em = emf.createEntityManager();
 
+        EmployeeCard employeeCard = em.find(EmployeeCard.class, 1L);
+        System.out.println("검색된 사원증 번호 : " + employeeCard.getCardId());
+        System.out.println("권한 : " + employeeCard.getRole());
+        System.out.println("사원증 소유자 : " + employeeCard.getEmployee().getName());
 
         Employee employee = em.find(Employee.class, 1L);
-//        System.out.println("사원증을 통한 직원 정보 접근 : " + employee.getCard().toString());
+        System.out.println("검색된 직원 이름 : " + employee.getName());
+        System.out.println("직원이 소유한 사원증 권한 : " + employee.getCard().getRole());
     }
 
     private static void dataInsert(EntityManagerFactory emf) throws ParseException {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
+        // 직원 등록
+        Employee employee = new Employee();
+        employee.setName("둘리");
+        em.persist(employee);
+
         // 사원증 등록
         EmployeeCard card = new EmployeeCard();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         card.setExpireDate(dateFormat.parse("2025-12-31"));
         card.setRole("MASTER");
+        // 직원에 대한 참조 설정
+        card.setEmployee(employee);
         em.persist(card);
-
-        // 직원 등록
-        Employee employee = new Employee();
-        employee.setName("둘리");
-        // 사원증에 대한 참조 설정
-//        employee.setEmployeeCard(card);
-        em.persist(employee);
-
 
         em.getTransaction().commit();
         em.close();
