a7cbed2dd7e0ba659191151c4240c0fd87db7561
tiktokyi <tiktokyi@gmail.com>
2024-07-04 22:30:42 +0900

    Add : 연결 클래스를 사용하지 않는 다대다 매핑

---
+++ src/main/java/org/example/chapter05/domain/Product.java
@@ -0,0 +1,22 @@
+package org.example.chapter05.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "S_PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;        // 상품 아이디
+
+    private String name;    // 상품 이름
+
+    @Column(name = "SHORT_DESC")
+    private String shortDesc;   // 상품 설명
+
+    private String category;    // 카테고리
+}
