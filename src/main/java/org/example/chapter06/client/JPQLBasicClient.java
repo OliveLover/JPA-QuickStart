64e9551e8f983a121f7963bf826b652993fc8384
tiktokyi <tiktokyi@gmail.com>
2024-07-10 14:56:14 +0900

    Mod : 상세 조회와 엔티티 캐시(객체 비교)

--- src/main/java/org/example/chapter06/client/JPQLBasicClient.java
+++ src/main/java/org/example/chapter06/client/JPQLBasicClient.java
@@ -5,8 +5,6 @@ import org.example.chapter06.domain.Employee;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import java.util.Arrays;
 import java.util.Date;
 
 public class JPQLBasicClient {
@@ -26,19 +24,15 @@ public class JPQLBasicClient {
     private static void dataSelect(EntityManagerFactory emf) {
         EntityManager em = emf.createEntityManager();
 
-        // JPQL
-        String jpql = "SELECT id, name, title, deptName, salary FROM Employee " +
-                "WHERE id = :employeeId AND name = :employeeName";
+        // 1번 직원 검색
+        Employee findEmp1 = em.find(Employee.class, 1L);
 
-        // JPQL 전송
-        Query query = em.createQuery(jpql);
-        query.setParameter("employeeId", 1L);
-        query.setParameter("employeeName", "직원 1");
+        // 1번 직원 검색
+        Employee findEmp2 = em.find(Employee.class, 1L);
 
-        // 검색 결과 처리
-        Object[] result = (Object[]) query.getSingleResult();
-        System.out.println(result[0] + "번 직원의 정보");
-        System.out.println(Arrays.toString(result));
+        if (findEmp1 == findEmp2) {
+            System.out.println("두 객체의 주소는 동일하다.");
+        }
 
         em.close();
     }
