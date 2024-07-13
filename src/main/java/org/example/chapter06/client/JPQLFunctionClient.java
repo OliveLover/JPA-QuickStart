e7c944fd9aea45e46db4446ac7a5a391690db52c
tiktokyi <tiktokyi@gmail.com>
2024-07-13 11:27:31 +0900

    Mod : 숫자형 함수

--- src/main/java/org/example/chapter06/client/JPQLFunctionClient.java
+++ src/main/java/org/example/chapter06/client/JPQLFunctionClient.java
@@ -27,14 +27,7 @@ public class JPQLFunctionClient {
     private static void dataSelect(EntityManagerFactory emf) {
         EntityManager em = emf.createEntityManager();
 
-        String jpql = "SELECT CONCAT(e.name, '의 급여 : ', e.salary) as 급여, "
-                + "     SUBSTRING(e.name, 1, 2), "
-                + "     TRIM(TRAILING '부' FROM e.dept.name), "
-                + "     LOWER(e.mailId), "
-                + "     UPPER(e.mailId), "
-                + "     LENGTH(e.mailId), "
-                + "     LOCATE('st', e.mailId) "
-                + "FROM Employee e";
+        String jpql = "SELECT ABS(-46), SQRT(9), MOD(20, 6) FROM Employee e";
         TypedQuery<Object[]> query = em.createQuery(jpql, Object[].class);
 
         List<Object[]> resultList = query.getResultList();
