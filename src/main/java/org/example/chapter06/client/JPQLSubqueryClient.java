1e2a50a492bc2186d212589dd4de903c067c3553
tiktokyi <tiktokyi@gmail.com>
2024-07-13 10:35:20 +0900

    Mod : 서브 쿼리 사용하기(메인 쿼리에서 직원 수가 3 이상인 부서 조회)

---
+++ src/main/java/org/example/chapter06/client/JPQLSubqueryClient.java
@@ -0,0 +1,89 @@
+package org.example.chapter06.client;
+
+import org.example.chapter06.domain.Department;
+import org.example.chapter06.domain.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class JPQLSubqueryClient {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter06");
+
+        try {
+            dataInsert(emf);
+            dataSelect(emf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            emf.close();
+        }
+    }
+
+    private static void dataSelect(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+
+        String jpql = "SELECT d FROM Department d "
+                + "WHERE (SELECT COUNT(e) "
+                + "     FROM Employee e "
+                + "     WHERE d.id = e.dept) >= 3";
+        TypedQuery<Department> query = em.createQuery(jpql, Department.class);
+
+        List<Department> resultList = query.getResultList();
+        System.out.println("소속된 직원이 3명 이상인 부서 목록");
+        for (Department department : resultList) {
+            System.out.println(department.getName());
+        }
+
+        em.close();
+    }
+
+    private static void dataInsert(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Department department1 = new Department();
+        department1.setName("개발부");
+        for (int i = 1; i <= 3; i++) {
+            Employee employee = new Employee();
+            employee.setName("개발직원 " + i);
+            employee.setSalary(i * 12700.00);
+            employee.setMailId("Dev-" + i);
+            employee.setDept(department1);
+        }
+        em.persist(department1);
+
+        Department department2 = new Department();
+        department2.setName("영업부");
+        for (int i = 1; i <= 3; i++) {
+            Employee employee = new Employee();
+            employee.setName("영원직원 " + i);
+            employee.setSalary(27300.00 * i);
+            employee.setMailId("Sale-" + i);
+            employee.setDept(department2);
+        }
+        em.persist(department2);
+
+        Department department3 = new Department();
+        department3.setName("인재개발부");
+        em.persist(department3);
+
+        // 부서 정보가 없는 새로운 직원 추가
+        Employee employee = new Employee();
+        employee.setName("아르바이트");
+        employee.setMailId("Alba-01");
+        employee.setSalary(10000.00);
+        em.persist(employee);
+
+        // 이름이 영업부인 새로운 직원 추가
+        Employee employee2 = new Employee();
+        employee2.setName("영업부");
+        em.persist(employee2);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
