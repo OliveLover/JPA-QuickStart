f85bdeabad14032a02d8597f5c3ecedb7d92d71e
Tiktokyi <tiktokyi@gmail.com>
2024-07-20 16:55:26 +0900

    Mod : 여러조건 사용하여 데이터 검색해보기

--- src/main/java/org/example/chapter08/biz/client/QueryMethodClient.java
+++ src/main/java/org/example/chapter08/biz/client/QueryMethodClient.java
@@ -27,8 +27,10 @@ public class QueryMethodClient {
 
     private static void dataSelect(EmployeeService employeeService) {
         Employee employee = new Employee();
-        employee.setName("개발직원 3");
+        employee.setName("개발");
+        employee.setMailId("Dev");
         List<Employee> resultList = employeeService.getEmployeeList(employee);
+        
         System.out.println("직원 목록");
         for (Employee result : resultList) {
             System.out.println("---> " + result.toString());
